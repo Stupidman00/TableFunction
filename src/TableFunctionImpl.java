f723d558d6b9f6f29696bb417042a6d70ef6562c
Stupidman00 <bva1138@yandex.ru>
2017-02-15 19:00:32 +0300

    equals(), hashCode(), toString().

--- src/TableFunctionImpl.java
+++ src/TableFunctionImpl.java
@@ -7,37 +7,37 @@ import java.util.Map;
  * Created by Admin on 11.02.2017.
  */
 public class TableFunctionImpl implements TableFunction {
-    private Map<Double, Double> values;
+    private final Map<Double, Double> table;
 
     TableFunctionImpl() {
-        values = new HashMap<>();
+        table = new HashMap<>();
     }
 
     @Override
     public void add(double x, double y) {
-        if (!values.containsKey(x)) values.put(x, y);
+        if (!table.containsKey(x)) table.put(x, y);
     }
 
     @Override
     public void remove(double x) {
-        values.remove(x);
+        table.remove(x);
     }
 
     @Override
     public Map<Double, Double> getTable() {
-        return values;
+        return table;
     }
 
     @Override
     public Pair<Double, Double> findNearestPoint(double x) throws Exception {
-        if (values.isEmpty()) throw new Exception("Table is empty.");
+        if (table.isEmpty()) throw new Exception("Table is empty.");
         Pair<Double, Double> result = new Pair<>(0.0, 0.0);
         double distance = 0.0;
-        for (double i : values.keySet()) {
-            if (i == x) return new Pair<>(i, values.get(i));
+        for (double i : table.keySet()) {
+            if (i == x) return new Pair<>(i, table.get(i));
             if (distance == 0.0 || Math.abs(i - x) < distance) {
                 distance = Math.abs(i - x);
-                result = new Pair<>(i, values.get(i));
+                result = new Pair<>(i, table.get(i));
             }
         }
         return result;
@@ -46,5 +46,27 @@ public class TableFunctionImpl implements TableFunction {
     @Override
     public double interpolate(double x) throws Exception {
         return findNearestPoint(x).getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TableFunctionImpl that = (TableFunctionImpl) o;
+
+        return table.equals(that.table);
+    }
+
+    @Override
+    public int hashCode() {
+        return table.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "TableFunctionImpl{" +
+                "table=" + table +
+                '}';
     }
 }
--- test/TableFunctionImplTest.java
+++ test/TableFunctionImplTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.*;
  * Created by Admin on 11.02.2017.
  */
 public class TableFunctionImplTest {
-    // TODO Необходимо ли тестировать методы add, remove, getTable?
     @Test
     public void findNearestPoint() throws Exception {
         TableFunction table = new TableFunctionImpl();
